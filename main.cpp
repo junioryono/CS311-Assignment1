e0f575be6ff2db8e890b2111d573f955c60c5274
Junior Yono <jryono123@gmail.com>
2021-03-03 23:58:30 -0800

    :)

--- main.cpp
+++ main.cpp
@@ -1,166 +1,199 @@
 // Name: Amer Yono
 // Student ID: 200711334
 // Email: yono004@cougars.csusm.edu
-#include <iostream>
-#define SIZE 10
-using namespace std;
 
-// Template stack class
-template <class Template>
-class Stack {
-    private:
-        Template array[SIZE];
-        int top;
-    public:
-        Stack();
-        Template push(Template);
-        Template pop();
-        Template getTop();
-        int isEmpty();
-        int isFull();
+#include <iostream>
+using std::cout, std::endl;
+
+// Node Structure
+struct Node {
+    int num;
+    Node* next;
 };
 
-template <class Template>
-Stack<Template>::Stack() {
-    // Set top to -1
-    top = -1;
+// Function that adds nodes to a linked list.
+void addToLinkedList(Node** linkedList, int numberToAdd) {
+    // Initialize and add new node
+    Node* newItem = new Node();
+
+    // Add values to the node
+    newItem->num = numberToAdd;
+    newItem->next = *linkedList;
+
+    // Add the node the linked list
+    *linkedList = newItem;
 }
 
-template <class Template>
-Template Stack<Template>::push(Template n) {
-    if (isFull()) return 0;
-    ++top;
-    array[top] = n;
-    return n;
+Node* unionLL(Node* LA, Node* LB) {
+    // Initialize unionizedLL
+    Node* unionizedLL = NULL;
+    
+    // Add both linked lists to unionizedLL
+    while (LA) {
+        addToLinkedList(&unionizedLL, LA->num);
+        LA = LA->next;
+    }
+
+    while (LB) {
+        addToLinkedList(&unionizedLL, LB->num);
+        LB = LB->next;
+    }
+
+    // Set pointer equal to unionizedLL
+    Node* unionizedPointer = unionizedLL;
+
+    // Create pointers for looping through unionizedPointer and removing duplicates
+    Node* insidePointer = NULL;
+    Node* duplicateNumber = NULL;
+    
+    // Go through the linked list by 1. The insidePointer will loop through every node during this.
+    while (unionizedPointer->next) {
+        insidePointer = unionizedPointer;
+
+        // Go through every unionizedPointer->num to check for duplicates
+        while (insidePointer->next) {
+            if (unionizedPointer->num == insidePointer->next->num) {
+                duplicateNumber = insidePointer->next;
+                insidePointer->next = insidePointer->next->next;
+                delete(duplicateNumber);
+            } else insidePointer = insidePointer->next;
+        }
+        unionizedPointer = unionizedPointer->next;
+    }
+
+    return unionizedLL;
 }
 
-template <class Template>
-Template Stack<Template>::pop() {
-    Template temp;
-    if(isEmpty()) return 0;
-    temp = array[top];
-    --top;
-    return temp;
+Node* sortLinkedList(Node* LL){
+    // Prevent recursion bugs
+	if (!LL) return LL;
+
+    // Set pointer equal to the linked list
+	Node* mergedPointer = LL;
+
+    // Set pointer equal to the next value in the linked list
+	Node* nextValuePointer = mergedPointer->next;
+
+    // Create pointers for the lower and higher numbers
+    Node* lowerNumber = NULL;
+	Node* higherNumber = NULL;
+
+    // Go through the linked list by 1. The insidePointer will loop through every node during this.
+	while (nextValuePointer) {
+	    Node* insidePointer = nextValuePointer->next;
+
+        // Go through every nextValuePointer->num to compare values
+		if (nextValuePointer->num < mergedPointer->num){
+            nextValuePointer->next = lowerNumber;
+            lowerNumber = nextValuePointer;
+		} else {
+            nextValuePointer->next = higherNumber;
+            higherNumber = nextValuePointer;
+		}
+		nextValuePointer = insidePointer;
+	}
+
+    // Sort the lower and higher numbers
+	lowerNumber = sortLinkedList(lowerNumber);
+	higherNumber = sortLinkedList(higherNumber);
+
+    // Go through all lower and higher numbers
+	if (lowerNumber) {
+	    Node* less_end = lowerNumber;
+	    while (less_end->next) less_end = less_end->next;
+
+        less_end->next = mergedPointer;
+        mergedPointer->next = higherNumber;
+
+        return lowerNumber;
+	} else mergedPointer->next = higherNumber;
+	
+    return mergedPointer;
 }
 
-template <class Template>
-Template Stack<Template>::getTop() {
-    return array[top];
+Node* mergeLL(Node* LA, Node* LB) {
+    // Initialize unionizedLL
+    Node* mergedLL = NULL;
+    
+    // Add both linked lists to unionizedLL
+    while (LA) {
+        addToLinkedList(&mergedLL, LA->num);
+        LA = LA->next;
+    }
+
+    while (LB) {
+        addToLinkedList(&mergedLL, LB->num);
+        LB = LB->next;
+    }
+
+    // Sort mergedLL
+    mergedLL = sortLinkedList(mergedLL);
+
+    return mergedLL;
 }
 
-template <class Template>
-int Stack<Template>::isEmpty() {
-    if (top == -1) return 1;
-    else return 0;   
-}
- 
-template <class Template>
-int Stack<Template>::isFull() {
-    if (top == SIZE - 1) return 1;
-    else return 0;
-}
-
-int orderOfOperations(char operation) {
-    if (operation == '+' || operation == '-') return 1;
-    if (operation == '*' || operation == '/') return 2;
-    return 0;
-}
-
-int operationCase(int numOne, int numTwo, char operation) {
-    switch (operation) {
-        case '+': return numOne + numTwo;
-        case '-': return numOne - numTwo;
-        case '*': return numOne * numTwo;
-        case '/': return numOne / numTwo;
+// Function that outputs all nodes in a linked list.
+void outputLinkedList(Node* node) {
+    // Check if a node exists
+    while (node) {
+        // If the next node does not exist, output two new lines instead of a comma
+        if (!node->next) cout << node->num << endl << endl;
+        else cout << node->num << ", ";
+    
+        // Set node equal to the next node
+        node = node->next;
     }
 }
 
-double evalExpression(const char* exp) {
-    // Convert to string to easily get length
-    const char* tempExp = exp;
-    std::string tempExpString = tempExp;
-
-    // Converts letters to decimal value
-    if (isdigit(*exp)) {
-        cout << "Problem: " << tempExpString << endl;
-
-        Stack<int> values;
-        Stack<char> operations;
-
-        for (int i = 0; i < tempExpString.length(); i++) {
-            // Check for spaces, division by 0, and parenthesis
-            if (exp[i] == ' ') continue;
-            if (exp[i] == '/' && (exp[i + 2]  == '0' || exp[i + 1]  == '0')) cout << "You cannot divide by 0." << endl;
-            
-            else if (exp[i] == '(') operations.push(exp[i]);
-            else if (isdigit(exp[i])) {
-                int value = 0;
-                while (i < tempExpString.length() && isdigit(exp[i])) {
-                    value = (value * 10) + (exp[i] - '0');
-                    i++;
-                }
-
-                values.push(value);
-                i--;
-            } else if (exp[i] == ')') {
-                while (!operations.isEmpty() && operations.getTop() != '(') {
-                    int valueTwo = values.getTop();
-                    values.pop();
-
-                    int valueOne = values.getTop();
-                    values.pop();
-
-                    char operation = operations.getTop();
-                    operations.pop();
-
-                    values.push(operationCase(valueOne, valueTwo, operation));
-                }
-
-                if (!operations.isEmpty()) operations.pop();
-            } else {
-                while (!operations.isEmpty() && orderOfOperations(operations.getTop()) >= orderOfOperations(exp[i])) {
-                    int valueTwo = values.getTop();
-                    values.pop();
-
-                    int valueOne = values.getTop();
-                    values.pop();
-
-                    char operation = operations.getTop();
-                    operations.pop();
-
-                    values.push(operationCase(valueOne, valueTwo, operation));
-                }
-                operations.push(exp[i]);
-            }
-        }
-
-        while (!operations.isEmpty()) {
-            int valueTwo = values.getTop();
-            values.pop();
-
-            int valueOne = values.getTop();
-            values.pop();
-
-            char operation = operations.getTop();
-            operations.pop();
-
-            values.push(operationCase(valueOne, valueTwo, operation));
-        }
-
-        cout << "Answer: " << values.getTop() << endl << endl;
-    } else cout << "Invalid Problem: " << tempExpString << endl << endl;
-}
-
 int main() {
-    // Test error cases as well as normal cases
-    evalExpression("10 * ( 10 + 1 + 3 ) /0");
-    evalExpression("10 * ( 10 + 1 + 3 ) / 0");
-    evalExpression("- - /2 ");
-    evalExpression("empty");
-    evalExpression("");
-    evalExpression("1 + 200");
-    evalExpression("1+200");
-    evalExpression("400/         200");
-    evalExpression("100 * ( 2 + 13 ) / 11");
+    // Initialize linkedLists
+    Node* linkedListA = NULL;
+    Node* linkedListB = NULL;
+    Node* linkedListUnion = NULL;
+    Node* linkedListMerge = NULL;
+
+    // Add elements to linkedListA
+    addToLinkedList(&linkedListA, 10);
+    addToLinkedList(&linkedListA, 16);
+    addToLinkedList(&linkedListA, 18);
+    addToLinkedList(&linkedListA, 21);
+    addToLinkedList(&linkedListA, 26);
+    
+    // Add elements to linkedListB
+    addToLinkedList(&linkedListB, 10);
+    addToLinkedList(&linkedListB, 13);
+    addToLinkedList(&linkedListB, 16);
+    addToLinkedList(&linkedListB, 17);
+    addToLinkedList(&linkedListB, 20);
+    addToLinkedList(&linkedListB, 23);
+
+    // Output all nodes in linkedListA
+    cout << "Linked List A:\n";
+    outputLinkedList(linkedListA);
+
+    // Output all nodes in linkedListB
+    cout << "Linked List B:\n";
+    outputLinkedList(linkedListB);
+
+    // Unionize linkedLists
+    linkedListUnion = unionLL(linkedListA, linkedListB);
+
+    // Output all nodes in linkedListUnion
+    cout << "Linked List Union:\n";
+    outputLinkedList(linkedListUnion);
+
+    // Merge linkedLists
+    linkedListMerge = mergeLL(linkedListA, linkedListB);
+
+    // Output all nodes in linkedListMerge
+    cout << "Linked List Merge:\n";
+    outputLinkedList(linkedListMerge);
+
+    // Free memory
+    delete(linkedListA);
+    delete(linkedListB);
+    delete(linkedListUnion);
+    delete(linkedListMerge);
+
     return 0;
 }
